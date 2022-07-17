2b1c1f1cab2fa75e56bcba506c62878bdf1e55fb
Vigneshwar M <vigneshwar221b@gmail.com>
2022-07-17 23:57:19 +0530

    takes input from env and cmd line args

--- backup.sh
+++ backup.sh
@@ -2,9 +2,35 @@
 
 main () {
   echo "db backup started..."
+
   day="$(date +'%A')"
-  db_backup="mydb_${day}.sql"
-  eval "mysqldump --all-databases -u username -p password > /opt/backups/${db_backup}"
+  db_backup="mydb_backup_${day}.sql"
+
+  username=""
+  password=""
+
+  # read from envs
+  if [[ -z "${mysql_uname}" ]]; then
+   username="default" 
+  else
+   username="${mysql_uname}"
+  fi
+
+  if [[ -z "${password}" ]]; then
+   password="default" 
+  else
+   password="${password}"
+  fi
+
+  # read from cmd line args
+  if [ "$#" -eq 0 ] ; then 
+   echo "no cmd line args are specified. Using the envs" 
+  else 
+    username=$1
+    password=$2
+  fi    
+
+  eval "mysqldump --all-databases -u $username -p $password > /opt/backups/${db_backup}"
   echo "db backup complete!"
 }
 
--- job.sh
+++ job.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 (crontab -l; echo "0 0 * * * ./backup.sh") | sort -u | crontab -
+
+# /var/spool/cron/crontabs/root
